e58d01a4238925855662bd4b9a646d65706b87c3
Kamil Stulgis <kamil.stulgis@k2.pl>
2021-04-12 23:59:48 +0200

    fix petli while oraz dodanie drugirgo rozwiazania

--- lab_3/zad8.py
+++ lab_3/zad8.py
@@ -1,12 +1,12 @@
 # Zdefiniuj funkcję, która zwraca sumę dowolnego ciągu arytmetycznego. Funkcja niech przyjmuje jako parametry: a1 (wartość początkowa), r (wielkość o ile rosną kolejne elementy) i ile_elementów (ile elementów ma sumować). Ponadto funkcja niech przyjmuje wartości domyślne: a1= 1, r=1, ile=10.
 
-def ciag(a1= 1, r=2, ile=10):
+def ciag(a1= 1, r=2, ile=3):
     i = 0
     if ile == 0:
         return 0.0
     else:
         suma = 0.0
-    while i <= ile:
+    while i < ile:
         suma += a1
         print(a1)
         a1 += r
@@ -18,4 +18,18 @@ print(ciag(2,3,4))
 print(ciag(6,2,3))
 print(ciag(1,1,1))
 print(ciag(-2,-1,5))
+
+
+# 2 rozwiazanie
+
+def suma(a1= 1, r=5, ile=3):
+    wynik=0
+    y=a1
+    for x in range(a1, ile+1):
+        wynik +=y
+        y +=r
+
+    return wynik
+
+print(suma())
 
