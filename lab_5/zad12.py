d63f1db6de28620fa9ff6ee54ec930cd6fc7d9fb
Kamil Stulgis <kamil.stulgis@k2.pl>
2021-04-14 15:35:04 +0200

    fix generator

--- lab_5/zad12.py
+++ lab_5/zad12.py
@@ -9,3 +9,19 @@ def rok_kalendarzowy():
 miesiace = rok_kalendarzowy()
 for miesiac in range(0, 12):
     print(next(miesiace))
+
+miesiace = rok_kalendarzowy()
+print(next(miesiace))
+print(next(miesiace))
+print(next(miesiace))
+print(next(miesiace))
+
+miesiace2 = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień']
+
+miesiac = (month for month in miesiace2)
+
+print(miesiac)
+print(next(miesiac))
+print(next(miesiac))
+print(next(miesiac))
+print(next(miesiac))
